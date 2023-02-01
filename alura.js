3f80fc4526de2796ae87f77a79bf65556cf46994
Matheus Gomes <matheusoutros@hotmail.com>
2023-02-01 12:11:00 -0300

    alterando o a descricao do Bitcoin

--- alura.js
+++ alura.js
@@ -1,3 +1,4 @@
+// Conversor de Dolar para Reais
 document.querySelector('#convertaDolar').addEventListener('click', function(event){
     event.preventDefault();
 
@@ -11,16 +12,17 @@ document.querySelector('#convertaDolar').addEventListener('click', function(even
 
 })
 
+// Conversor de Reais para Bitcoin
 document.querySelector('#convertaCripto').addEventListener('click', function(event){
     event.preventDefault();
 
-    let valorEmdolar = document.querySelector('#valorDoClienteEmCripto').value;
-    let cotacaoDoDolar = 23157.80;
+    let valorEmReais = document.querySelector('#valorDoClienteEmCripto').value;
+    let bitcoin = 23000.00;
 
-    let valorEmReal = valorEmdolar * cotacaoDoDolar;
-        valorEmReal = valorEmReal.toFixed(2)
+   
+    let valorEmReal = valorEmReais / bitcoin;
+        valorEmReal = valorEmReal.toFixed(8)
 
     document.querySelector('.valorConvertidoEmCripto').innerHTML = valorEmReal
 
 })
-
